e584b318951b61bb14bf0b74c91b8fef55d32cef
kejt <kgrzevic@yahoo.com>
2014-04-26 18:51:09 +0200

    Commit 5

--- hello_world.php
+++ hello_world.php
@@ -15,15 +15,20 @@ class Foo {
   function someFunction($param,$param2) {
     echo $param.' '.$param2.$this->bar."<br/>";  
   }
-
+  function someFunction2($param,$param2) {
+    echo $param.' '.$param2.$this->bar."<br/>";  
+  }
 }
 
 $foo = new Foo();
 
 $foo->setBar("Charles","Dickens");
+
 $foo->someFunction("Hello, ");
 $foo->someFunction("Godbye, ");
 
+$foo->someFunction2("Hello, ");
+$foo->someFunction2("Godbye, ");
 ?>
 </body>
 </html>
